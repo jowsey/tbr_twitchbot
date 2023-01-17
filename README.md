8cc6e5e84b6e4e2f90d62345ce6fd67845990b43
Thomas Jowsey <jow20003347@wakefield.ac.uk>
2023-01-17 01:19:29 +0000

    Use OBS ws, default env file, intermission catchup

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Uses [pnpm](https://pnpm.io), set up with `pnpm install` & run with `pnpm start`
 Expects tokens from the [Twitch dev console](https://dev.twitch.tv/console/) and the [Twitch CLI](https://github.com/twitchdev/twitch-cli).  
 Use something like [SQLite Browser](https://sqlitebrowser.org/) to add the initial keys  
 Enter your channel name, client id etc in the `.env` file created on first run
+
+Connects to OBS for updating the scene with game info
--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,17 @@ import { TokensRow } from "./TokensRow";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import OBSWebSocket from "obs-websocket-js";
+
 import { RefreshingAuthProvider } from "@twurple/auth";
 import { ChatClient } from "@twurple/chat";
 
 import { WebSocketServer } from "ws";
-import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import Database from "better-sqlite3";
 
-if (!existsSync(".env")) {
-  writeFileSync(
-    ".env",
-    '# https://dev.twitch.tv/console\nBOT_USERNAME=""\nCHANNEL_NAME=""\n\nCLIENT_ID=""\nCLIENT_SECRET=""\n'
-  );
+if (!existsSync(".env") || (existsSync(".env") && readFileSync(".env").length === 0)) {
+  writeFileSync(".env", readFileSync("src/default.env"));
   throw new Error("No .env file found - an empty file has been created, please fill it!");
 }
 
@@ -40,11 +39,16 @@ if (tokens === undefined) {
   throw new Error("Please add initial tokens to database");
 }
 
-let isRoundIntermission = true;
+let isRoundIntermission = false;
 let isRoundInProgress = false;
 
 let playersQueuedForRound: PlayerDetails[] = [];
 
+// Connect to OBS for handling scene stuff
+const obs = new OBSWebSocket();
+await obs.connect(`ws://127.0.0.1:${process.env.OBS_PORT}`, process.env.OBS_PASSWORD);
+
+// Connec to Twitch auth
 const authProvider = new RefreshingAuthProvider(
   {
     clientId: process.env.CLIENT_ID!,
@@ -66,6 +70,7 @@ const authProvider = new RefreshingAuthProvider(
   }
 );
 
+// Connect to Twitch IRC
 const chatClient = new ChatClient({ authProvider, channels: [process.env.CHANNEL_NAME!] });
 
 chatClient.onRegister(() => {
@@ -105,20 +110,11 @@ chatClient.onMessage(async (channel, user, message, msg) => {
 
 await chatClient.connect();
 
+// Connect to local WS server
 const wss = new WebSocketServer({ port: 1949 });
 
 wss.on("connection", (ws) => {
-  // if someone somehow manages to !play before the map launched
-  if (playersQueuedForRound.length > 0) {
-    ws.send(
-      JSON.stringify({
-        event: "catchup",
-        players: playersQueuedForRound,
-      })
-    );
-  }
-
-  ws.on("message", (data) => {
+  ws.on("message", async (data) => {
     console.log("received", data.toString());
 
     let d = JSON.parse(data.toString());
@@ -127,6 +123,16 @@ wss.on("connection", (ws) => {
       case "beginIntermission": {
         isRoundInProgress = false;
         isRoundIntermission = true;
+
+        // if someone manages to !play before the map launches
+        if (playersQueuedForRound.length > 0) {
+          ws.send(
+            JSON.stringify({
+              event: "catchup",
+              players: playersQueuedForRound,
+            })
+          );
+        }
         break;
       }
 
@@ -135,6 +141,28 @@ wss.on("connection", (ws) => {
         isRoundInProgress = true;
 
         playersQueuedForRound = [];
+        break;
+      }
+
+      case "updateOBSPrimaryText": {
+        await obs.call("SetInputSettings", {
+          inputName: process.env.OBS_PRIMARY_TEXT_SOURCE!,
+          inputSettings: {
+            text: d.text,
+          },
+        });
+
+        break;
+      }
+
+      case "updateOBSSecondaryText": {
+        await obs.call("SetInputSettings", {
+          inputName: process.env.OBS_SECONDARY_TEXT_SOURCE!,
+          inputSettings: {
+            text: d.text,
+          },
+        });
+
         break;
       }
     }
